c36cb9cafff3c0152bbc06f8e004b9e5dfaf3185
Fabian Dammekens <fabian.dammekens@cegeka.be>
2020-01-29 18:03:42 +0100

    As a hosting provider I want to use environment variables so that i can dynamically assign database config to the application

--- OnRent/inc/config.php
+++ OnRent/inc/config.php
@@ -1,4 +1,4 @@
 <?php
 session_start(); /* Session */
-$con=mysqli_connect("localhost","root","","rentalw"); /*Database Connection*/
+$con=mysqli_connect($_ENV["DATABASE_SERVER"],$_ENV["DATABASE_USER"],$_ENV["DATABASE_PASSWORD"],$_ENV["DATABASE_NAME"]); /*Database Connection*/
 ?>
